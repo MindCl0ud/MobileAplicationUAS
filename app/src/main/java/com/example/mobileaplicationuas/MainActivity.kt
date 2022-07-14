053ed44fa3fc8d6e6a8335b317dc9a90c8b17099
MindCl0ud <ahmadagilsusanto@gmail.com>
2022-07-14 10:06:02 +0800

    Initial commit

---
+++ app/src/main/java/com/example/mobileaplicationuas/MainActivity.kt
@@ -0,0 +1,48 @@
+package com.example.mobileaplicationuas
+
+import android.os.Bundle
+import android.os.Message
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.mobileaplicationuas.ui.theme.MobileAplicationUASTheme
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.dp
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            MobileAplicationUASTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colors.background
+                ) {
+                    Greeting("Android")
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String) {
+    Text(text = "Hello $name!")
+}
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    MobileAplicationUASTheme {
+        Greeting("Android")
+    }
+}
